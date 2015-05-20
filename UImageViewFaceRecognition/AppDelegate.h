ef0efeecc9ee126dc3669112d3cac4c78854039d
Jesus Antonio Gil <jesus.a22@hotmail.com>
2015-05-20 11:09:17 +0200

    First commit

---
+++ UImageViewFaceRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UImageViewFaceRecognition
+//
+//  Created by Jesus Antonio Gil on 20/5/15.
+//  Copyright (c) 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UImageViewFaceRecognition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UImageViewFaceRecognition
+//
+//  Created by Jesus Antonio Gil on 20/5/15.
+//  Copyright (c) 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
