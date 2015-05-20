9b725efa7138aeb2e0c785826f6bb214a7ecec47
Jesus Antonio Gil <jesus.a22@hotmail.com>
2015-05-20 12:13:58 +0200

    UIImage recognition created

---
+++ UImageViewFaceRecognition/UIImageView+FaceRecognition/UIImageView+FaceRecognition.m
@@ -0,0 +1,14 @@
+//
+//  UIImageView+FaceRecognition.m
+//  UImageViewFaceRecognition
+//
+//  Created by Jesus Antonio Gil on 20/5/15.
+//  Copyright (c) 2015 Jesus Antonio Gil. All rights reserved.
+//
+
+#import "UIImageView+FaceRecognition.h"
+
+
+@implementation UIImageView (FaceRecognition)
+
+@end
